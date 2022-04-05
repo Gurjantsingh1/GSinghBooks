f2b933e0f5d1f2ef16b5bc73cca0ca119da91cc4
Gurjantsingh1 <90393816+Gurjantsingh1@users.noreply.github.com>
2022-04-06 03:30:27 +0530

    got error in last file now doing next steps

--- GSinghBooks/Areas/Admin/Controllers/CategoryController.cs
+++ GSinghBooks/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Gsingh.DataAccess.Repository.IRepository;
+using Gsingh.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,21 +12,28 @@ namespace GSinghBooks.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+
         public CategoryController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
-
-      public IActionResult Index()
-      {
-            return View();
-      }
-		#region API CALLS
-        public IAuctionResult GetAll()
+        public IActionResult Index()
         {
-            var allObj = _unitOfWork.Category.GetAll();
-            return Json(new { data = allObj });
+            return View();
         }
-		#endregion
-	}
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                return View(category);
+            }
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+    }
 }
